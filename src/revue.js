95dda262cbfa76d4eb8474e10511001f4541f57c
egoist <0x142857@gmail.com>
2015-11-27 18:08:56 +0800

    clean

--- src/revue.js
+++ src/revue.js
@@ -1,7 +1,8 @@
 export default function (Vue, options) {
   const _ = Vue.util
+  const store = options.store
   // bring redux to revue
-  _.defineReactive(Vue.prototype, '$revue', options.store)
+  _.defineReactive(Vue.prototype, '$revue', store)
   // listen for state changes
   _.defineReactive(Vue.prototype, '$subscribe', function () {
     const self = this
@@ -10,14 +11,14 @@ export default function (Vue, options) {
     props.forEach(prop => {
       let currentValue
       function handleChange() {
-        let previousValue = currentValue;
-        currentValue = options.store.getState()[prop]
+        let previousValue = currentValue
+        currentValue = store.getState()[prop]
 
         if (previousValue !== currentValue) {
           self.$set(prop, currentValue)
         }
       }
-      self.unsubscriber.push(options.store.subscribe(handleChange))
+      self.unsubscriber.push(store.subscribe(handleChange))
     })
   })
   _.defineReactive(Vue.prototype, '$unsubscribe', function () {
