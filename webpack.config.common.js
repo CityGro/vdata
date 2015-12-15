6cdaee9ad4f136e94cd16ab213f113c1e988688d
egoist <0x142857@gmail.com>
2015-12-15 18:25:43 +0800

    directly use deps in package.json

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,17 +1,13 @@
 var webpack = require('webpack')
-var path = require('path');
-var fs = require('fs');
+var path = require('path')
+var fs = require('fs')
+var pkg = require('./package')
 var config = require('./webpack.config.umd')
 
 var nodeModules = {}
-fs.readdirSync('node_modules')
-  .filter(function(x) {
-    return ['.bin'].indexOf(x) === -1
-  })
-  .forEach(function(mod) {
-    nodeModules[mod] = 'commonjs ' + mod
-  })
-
+for (var mod in pkg.dependencies) {
+  nodeModules[mod] = 'commonjs ' + mod
+}
 
 config.output.libraryTarget = 'commonjs2'
 config.output.filename = 'revue.common.js'
