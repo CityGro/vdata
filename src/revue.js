67508b8ccc64c54e8a4df593894b0cfb8707a0f7
EGOIST <0x142857@gmail.com>
2016-02-01 10:40:53 +0800

    bindVue should be an internal function

--- src/revue.js
+++ src/revue.js
@@ -3,10 +3,53 @@ import {set as setProp, get as getProp} from 'object-path'
 
 const re = /^([a-zA-Z0-9\._-]+)\s{1,2}as\s{1,2}([a-zA-Z0-9\._-]+)$/i
 
+/**
+ * Bind reduxStore to Vue instance
+ *
+ * @param {Vue} Vue
+ * @param {object} store - redux store
+ */
+function bindVue(Vue, store) {
+  Vue.prototype.$subscribe = function (...args) {
+    if (this._calledOnce) {
+      if (process.env.NODE_ENV === 'production') {
+        return false
+      }
+      throw new Error('[Revue] You can only subscribe once, pass multi args to subscribe more than one state.')
+    }
+    this._calledOnce = true
+    this._unsubscribers = []
+    args.forEach(prop => {
+      // realProp: property name/path in your instance
+      // storeProp: property name/path in Redux store
+      let realProp = prop,
+        storeProp = prop
+      if (re.test(prop)) {
+        [, storeProp, realProp] = prop.match(re)
+      }
+      let currentValue = getProp(store.getState(), storeProp)
+      const handleChange = () => {
+        let previousValue = currentValue
+        currentValue = getProp(store.getState(), storeProp)
+        if (!shallowEqual(previousValue, currentValue)) {
+          setProp(this._data, realProp, currentValue)
+        }
+      }
+      this._unsubscribers.push(store.subscribe(handleChange))
+    })
+  }
+  Vue.prototype.$unsubscribe = function () {
+    if (this._unsubscribers && this._unsubscribers.length > 0) {
+      this._calledOnce = false
+      this._unsubscribers.forEach(un => un())
+    }
+  }
+}
+
 export default class Revue {
   constructor(Vue, reduxStore) {
     this.store = reduxStore
-    this.bindVue(Vue)
+    bindVue(Vue, this.store)
   }
   get state() {
     return this.store.getState()
@@ -29,42 +72,5 @@ export default class Revue {
       }
     }
     return actions
-  }
-  bindVue(Vue) {
-    const store = this.store
-    Vue.prototype.$subscribe = function (...args) {
-      if (this._calledOnce) {
-        if (process.env.NODE_ENV === 'production') {
-          return false
-        }
-        throw new Error('[Revue] You can only subscribe once, pass multi args to subscribe more than one state.')
-      }
-      this._calledOnce = true
-      this._unsubscribers = []
-      args.forEach(prop => {
-        // realProp: property name/path in your instance
-        // storeProp: property name/path in Redux store
-        let realProp = prop,
-          storeProp = prop
-        if (re.test(prop)) {
-          [, storeProp, realProp] = prop.match(re)
-        }
-        let currentValue = getProp(store.getState(), storeProp)
-        const handleChange = () => {
-          let previousValue = currentValue
-          currentValue = getProp(store.getState(), storeProp)
-          if (!shallowEqual(previousValue, currentValue)) {
-            setProp(this._data, realProp, currentValue)
-          }
-        }
-        this._unsubscribers.push(store.subscribe(handleChange))
-      })
-    }
-    Vue.prototype.$unsubscribe = function () {
-      if (this._unsubscribers && this._unsubscribers.length > 0) {
-        this._calledOnce = false
-        this._unsubscribers.forEach(un => un())
-      }
-    }
   }
 }
