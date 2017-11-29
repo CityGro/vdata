677709c4fae7dcaeadb98b1b38cd95b2399e458c
Carlos Killpack <carlos@citygro.com>
2017-11-29 10:12:22 -0700

    switch to babel-env, setup eslint

--- rollup.conf.cjs.js
+++ rollup.conf.cjs.js
@@ -19,16 +19,29 @@ export default {
       babelrc: false,
       exclude: 'node_modules/**',
       presets: [
-        [
-          "es2015",
-          {
-            "modules": false
-          }
-        ],
-        "stage-2"
+        ["env",
+         {
+           modules: false,
+           targets: {
+             browsers: [
+               '> 5% in US',
+               'Firefox ESR',
+               'last 2 versions'
+             ]
+           },
+           include: [
+             'transform-regenerator',
+             'transform-es2015-spread'
+           ]
+         }
+        ]
       ],
       plugins: [
-        'external-helpers'
+        'external-helpers',
+        'syntax-async-functions',
+        'syntax-flow',
+        'transform-flow-comments',
+        'transform-object-rest-spread'
       ]
     })
   ],
